7b853e913cdf239a01b4d230d1e285e419365657
mark-evans-mantis <mark@mantis.works>
2025-12-09 14:02:01 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.html",
+    "./public/**/*.html"
   ],
   theme: {
     extend: {},
